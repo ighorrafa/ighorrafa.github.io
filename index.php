68349579eda6b2df227f5ec291fab5f0e6fd0c71
ighorrafa <47404458+ighorrafa@users.noreply.github.com>
2021-03-14 22:18:50 -0300

    change to php format

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,5 +9,3 @@
     <p>Teste</p>
     <a href="../ighorrafa.github.io/projects/kool-poke/">Veja mais</a>
 </body>
-
-</html>
