1ad83ca9f414f6b7e55fb15486ab90ee5c94d9ef
ighorrafa <47404458+ighorrafa@users.noreply.github.com>
2021-05-27 23:32:36 -0300

    color select

--- assets/js/buttons-custom.js
+++ assets/js/buttons-custom.js
@@ -1,8 +1,8 @@
 function changeColor(color) {
-    $('body').attr('data-color', color)
-}
+    $('body').attr('data-color', color);
+};
 
 $('.colors li').on("click", function() {
     className = $(this).attr('class');
     changeColor(className);
-})
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,31 @@ $(window).load(function() {
     startSite();
     AOS.init();
 });
+
+$(window).scroll(function() {
+    var vh = $(window).height();
+    var posY = $(window).scrollTop();
+    var display = $('#sec-04').offset().top + 150;
+    var endPoint = $('.endpoint').offset().top - vh;
+    if (posY > display) {
+        $('.border-display').css("position", "fixed");
+    }
+    if (posY > endPoint) {
+        $('.border-display').css({ "position": "absolute", "bottom": "0", "top": "unset" });
+    } else if (posY < display) {
+        $('.border-display').css({ "position": "absolute", "top": "0" });
+    }
+});
+
+var lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function() { // or window.addEventListener("scroll"....
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop) {
+        console.log('descendo');
+    } else {
+        console.log('subindo');
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
