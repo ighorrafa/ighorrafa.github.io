6d6867762be9a60ea50cac4536b9279da8ed8fd8
Ighor Rafael <ighor@navas.design>
2023-07-06 11:09:51 -0300

    cores dos shapes

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,7 @@ particlesJS("particles-js", {
       type: "circle",
       stroke: {
         width: 0,
-        color: "#000000",
+        color: "#ffffff",
       },
       polygon: {
         nb_sides: 5,
