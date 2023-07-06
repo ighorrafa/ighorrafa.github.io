beb617d720d17d1c74ddaf064fd9d4f1c531520c
Ighor Rafael <ighor@navas.design>
2023-07-06 11:03:22 -0300

    ajustes de cor e texto

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,7 @@ particlesJS("particles-js", {
       },
     },
     color: {
-      value: "#ffffff",
+      value: "#000000",
     },
     shape: {
       type: "circle",
@@ -131,7 +131,7 @@ particlesJS("particles-js", {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#ffffff",
+      color: "#000000",
       opacity: 0.4,
       width: 1,
     },
